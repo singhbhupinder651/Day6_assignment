abcbb5ecb37f477633df1b6b3e3267454cc82795
singhbhupinder651 <71844883+singhbhupinder651@users.noreply.github.com>
2022-10-08 16:51:20 +0530

    updated

---
+++ CouponNumbersProblem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6LogicalPrograms
+{
+    internal class CouponNumbersProblem
+    {
+        public static void CouponNumber(int N)
+        {
+
+            int[] array = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                Random random = new Random();
+                int Random_generate = random.Next(999, 10000);
+                if (i == 0)
+                {
+                    array[i] = Random_generate;
+                }
+                else
+                {
+                    int j = 0;
+                    int check = 0;
+                    while (j < i)
+                    {
+                        if (array[j] == Random_generate)
+                            check = 1;
+                        j++;
+                    }
+                    if (check == 0)
+                        array[i] = Random_generate;
+                    else
+                        i--;
+                }
+            }
+            Console.WriteLine("Distinct Coupons Are:");
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write($" {array[i]}");
+            }
+        }
+    }
+}
---
+++ ReverseNumberProblem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6LogicalPrograms
+{
+    internal class ReverseNumberProblem
+    {
+        public void ReverseNumber()
+        {
+            int reverse = 0;
+            int remainder = 0;
+
+            Console.WriteLine("Enter the number to Reverse");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            while (num != 0)
+            {
+                remainder = num % 10;
+                reverse = reverse * 10 + remainder;
+                num /= 10;
+            }
+            Console.WriteLine("Reversed number is :" + reverse);
+        }
+    }
+}
